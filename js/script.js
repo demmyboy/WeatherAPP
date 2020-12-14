0891f0561c72596121ee0311f24091e87d018f9d
Demmy <demolaabdulai@live.com>
2020-12-14 06:02:59 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const search = document.getElementById('search')
+const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=`
+const form = document.getElementById('form')
+const main = document.getElementById('main')
+
+
+
+async function getCityTemp() {
+    const city = search.value
+    const res = await axios.get(`${API_URL}${city}&appid=13515bb020bd78b28c20e336ca557eac`)
+    createCityCard(res.data)
+    console.log(res.data)
+}
+
+function createCityCard(city) {
+    const cardHTML = `
+    <div class="cards">
+        <div class="weather-info">
+            <div id="city" class="mb-3">
+                <h3><span>CITY : </span> ${city.name} </h3>
+            </div>
+            <div id="country" class="mb-3">
+                <h3><span>COUNTRY : </span> ${city.sys.country} </h3>
+            </div>
+            <div id="temperature" class="mb-3">
+                <h3><span>TEMP : </span> ${Math.round(kelvinToCelcius(city.main.temp))}
+                    <sup>0</sup>C
+                </h3>
+            </div>
+        </div>
+    </div>
+    
+    `
+    main.innerHTML = cardHTML
+}
+
+function kelvinToCelcius(kelvin) {
+    const celcius = kelvin - 273.15
+    return celcius
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const searchcity = search.value
+    if (searchcity) {
+        getCityTemp()
+        search.value = ''
+    }
+})
+
+getCityTemp()
